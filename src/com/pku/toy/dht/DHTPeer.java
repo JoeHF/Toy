4eab8d4de5e469fb0534154c32d6a03ba7e43dcf
unknown <zeyazhang26@gmail.com>
2015-06-20 17:13:11 +0800

    message13

--- src/com/pku/toy/dht/DHTPeer.java
+++ src/com/pku/toy/dht/DHTPeer.java
@@ -1,20 +1,12 @@
 package com.pku.toy.dht;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
+import java.io.*;
 import java.net.MalformedURLException;
 import java.rmi.Naming;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.util.*;
 
-import Dataset.SPair;
 
 public class DHTPeer implements IDHTPeer
 {
