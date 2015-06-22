8cc098eeae097cd2383a0c9a071dc6bc0428d3e8
houfang <707980114@qq.com>
2015-06-22 09:24:40 +0800

    获取地址修改

--- src/util/Address.java
+++ src/util/Address.java
@@ -18,8 +18,8 @@ public class Address {
 		    {
 		        InetAddress i = (InetAddress) ee.nextElement();
 		        String[] temp = i.getHostAddress().split("\\.");
-		        
-		        if (temp.length == 4 && (!temp[0].equalsIgnoreCase("127"))) {
+		        System.out.println(i.getHostAddress());
+		        if (temp.length == 4 && (temp[0].equalsIgnoreCase("162") || temp[0].equalsIgnoreCase("177"))) {
 					ip = i.getHostAddress();
 				}
 		    }
